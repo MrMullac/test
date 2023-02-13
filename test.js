b76d82ab358b63b057899b3a9ae8ff6d853756d2
Callum Watson <66056978+MrMullac@users.noreply.github.com>
2023-02-13 22:28:24 +1300

    Create test.js

---
+++ test.js
@@ -0,0 +1,41 @@
+<script type="text/javascript">
+      new FinisherHeader({
+        "count": 90,
+        "size": {
+          "min": 1,
+          "max": 20,
+          "pulse": 0
+        },
+        "speed": {
+          "x": {
+            "min": 0,
+            "max": 0.4
+          },
+          "y": {
+            "min": 0,
+            "max": 0.1
+          }
+        },
+        "colors": {
+          "background": "#0c0f14",
+          "particles": [
+            "#ffffff",
+            "#87ddfe",
+            "#acaaff",
+            "#1bffc2",
+            "#f88aff"
+          ]
+        },
+        "blending": "screen",
+        "opacity": {
+          "center": 0,
+          "edge": 0.4
+        },
+        "skew": -2,
+        "shapes": [
+          "c",
+          "s",
+          "t"
+        ]
+      });
+      </script>
